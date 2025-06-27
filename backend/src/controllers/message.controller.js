57a9deec6a1246e8f6a5fa500c0d4a66ac10fd4c
Sanyam Munot <sanyammunot03@gmail.com>
2025-06-28 00:14:21 +0530

    initial commit - with user side done and working on msg controller

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,43 @@
+
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: { $ne: loggedInUserId } }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("Error in getUsersForSidebar: ",error.message);
+        res.status(400).json({error: "Internal server error"});
+    }
+};
+
+export const getMessages = async (req,res) => {
+    try {
+        const { id: userToChatId }= req.params;
+        const senderId = req.user._id;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId: senderId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: senderId}
+            ]
+        })
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages: ",error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+};
+
+export const sendMessage = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in sendMessage: ",error.message);
+        res.status(500).json({error: "Internal server error"});
+    } 
+};
