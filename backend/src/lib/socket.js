716d5c6304c2d41beab1424e227ae9a6304c6770
Sanyam Munot <117111306+sanyammunot@users.noreply.github.com>
2025-07-12 17:32:20 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://chatapplication-frontend-z99n.onrender.com"]
+        origin: ["http://localhost:5173", "https://chatapplication-frontend-z99n.onrender.com"],
+        credentials: true
     },
 });
 
