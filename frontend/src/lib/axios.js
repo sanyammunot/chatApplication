c536c3c115d98cdda571533cdf458460c5a01473
Sanyam Munot <117111306+sanyammunot@users.noreply.github.com>
2025-07-12 17:14:47 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chatapp-8vyo.onrender.com",
+    baseURL: "https://chatapp-8vyo.onrender.com/api",
     withCredentials: true,
 });
