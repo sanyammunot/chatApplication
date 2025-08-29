4a55a3e658ae29d376cf965a14181df0c0b51fff
Sanyam Munot <117111306+sanyammunot@users.noreply.github.com>
2025-08-29 22:46:11 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -86,7 +86,7 @@ export const useAuthStore = create((set, get) => ({
         const {authUser} = get();
         if(!authUser || get().socket?.connected)
             return ;
-        const socket = io(BASE_URL, {
+        const socket = io("https://chatapp-8vyo.onrender.com", {
             withCredentials: true,
             query: {
                 userId: authUser._id,
