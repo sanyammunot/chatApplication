977a41709b2d40288030fd728d14e8e85c9d19ff
Sanyam Munot <117111306+sanyammunot@users.noreply.github.com>
2025-09-01 22:18:03 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,9 @@ export const generateToken = (userId,res) => {
     res.cookie("jwt",token, {
         maxAge: 7*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        domain: "https://chat-application-tan-one.vercel.app",
+        //sameSite: "strict",
+        secure: true
     });
     return token;
 };
